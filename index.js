fcca00d150409d0c233636759071e3305d55bd9b
timpel301 <tim.pel301@gmail.com>
2023-06-17 23:38:44 +0200

    backbone of exercise

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
+const bodyparser = require('body-parser');
+const mongoose = require('mongoose');
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,9 +11,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(bodyparser.toString());
+mongoose.connect(process.env.MONGO_DB, { useNewUrlParser: true, useUnifiedTopology: true });
 
+app.route('api/users').get((req,res) =>{
 
+}).post((req,res,next) => {
 
+});
+
+app.post('/api/users/:_id/exercises', (req,res,next) => {
+
+});
+
+app.get('api/users/:_id/logs?[from][&to][&limit]', (req,res) => {
+
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
